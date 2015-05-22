8097bcc913bd1b1fc80dce2ffeef02cb55b3c317
kcrimi <kevincrimi@gmail.com>
2015-05-22 15:35:50 -0400

    Retain on rotation with fragments

--- app/src/main/java/com/example/kevin/instacam/NewPhotoActivity.java
+++ app/src/main/java/com/example/kevin/instacam/NewPhotoActivity.java
@@ -16,40 +16,44 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
 
-public class NewPhotoActivity extends ActionBarActivity {
+public class NewPhotoActivity extends ActionBarActivity implements NewPhotoFragment.Contract{
 
     private static final int CAMERA_REQUEST = 10;
+    private static final String PHOTO_STATE_EXTRA = "PHOTO";
     public static final String PHOTO_EXTRA = "PHOTO_EXTRA";
+
     private Photo mPhoto;
-    private ImageView mPreview;
+    private NewPhotoFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_photo);
 
-        mPreview = (ImageView)findViewById(R.id.photo_preview);
-        final EditText caption = (EditText)findViewById(R.id.new_photo_caption);
+        mFragment  = (NewPhotoFragment)getFragmentManager().findFragmentById(R.id.new_photo_fragment_container);
+        if(mFragment == null){
+            mFragment = new NewPhotoFragment();
 
-        Button saveButton = (Button)findViewById(R.id.save_new_photo);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPhoto.setCaption(caption.getText().toString());
-                Intent i = new Intent();
-                i.putExtra(PHOTO_EXTRA, mPhoto);
-                setResult(RESULT_OK, i);
-                finish();
-            }
-        });
-        launchCamera();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.new_photo_fragment_container, mFragment)
+                    .commit();
+        }
     }
 
-    private void launchCamera(){
+    @Override
+    public void launchCamera(){
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         mPhoto = new Photo();
         i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhoto.getFile()));
         startActivityForResult(i, CAMERA_REQUEST);
+    }
+
+    @Override
+    public void finishedPhoto(Photo photo) {
+        Intent i = new Intent();
+        i.putExtra(PHOTO_EXTRA, photo);
+        setResult(RESULT_OK, i);
+        finish();
     }
 
     @Override
@@ -78,8 +82,7 @@ public class NewPhotoActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST){
             if(resultCode == RESULT_OK) {
-                Log.d("New Photo", "Result received");
-                Picasso.with(this).load(mPhoto.getFile()).into(mPreview);
+                mFragment.updatePhoto(mPhoto);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);

c9d61fcb3e190c80b92f0a4f9626c9f4611a2fad
kcrimi <kevincrimi@gmail.com>
2015-05-22 19:35:45 -0400

    facebook login working

---
+++ app/src/main/java/com/example/kevin/instacam/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.example.kevin.instacam;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private static final String TAG = "LoginActivity";
+    private UiLifecycleHelper mUiLifecycleHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUiLifecycleHelper = new UiLifecycleHelper(this, new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState sessionState, Exception e) {
+                onSessionStateChanged(session, sessionState, e);
+            }
+        });
+    }
+
+    private void onSessionStateChanged(Session session, SessionState sessionState, Exception e){
+        if (sessionState.isOpened()){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }else{
+            Log.d(TAG, "Session Closed");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mUiLifecycleHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mUiLifecycleHelper.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mUiLifecycleHelper.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mUiLifecycleHelper.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mUiLifecycleHelper.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
